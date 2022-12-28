749300ca46fc9ca9252443f49e9eaa57543e9ce3
oznzrl <oznzrl@gmail.com>
2022-12-28 17:35:25 +0300

    a

--- src/ethereum/storage/Storage.sol
+++ src/ethereum/storage/Storage.sol
@@ -1,6 +1,7 @@
-pragma solidity >=0.8 ;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
-contract simple {
+contract Storage{
 
   
 struct Nav{
@@ -65,23 +66,4 @@ emit NavDataUpdated(NAV_IDENT);
     value = newValue;
     emit StorageUpdate(msg.sender,value);
   }
-
-    mapping(string => int) private accounts;
-      string  acc;
-    function open(string memory acc_id, int amount) public {
-        accounts[acc_id] = amount;
-        acc=acc_id;
-    }
-    function getacc() public view returns(string memory){
-        return acc;
-    }
-
-    function query(string memory acc_id) public view returns (int amount) {
-        amount = accounts[acc_id];
-    }
-
-    function transfer(string memory acc_from, string memory acc_to, int amount) public {
-        accounts[acc_from] -= amount;
-        accounts[acc_to] += amount;
-    }
 }
